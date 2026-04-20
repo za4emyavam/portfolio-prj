a69266217b99393b384143e52c0034adb0de014e
za4emyavam <za4emyavam@gmail.com>
2026-04-20 22:33:33 +0200

    infra: move terraform state from local to hcp terraform

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,15 @@
 provider "aws" {
   region = "eu-central-1"
 }
+
+terraform {
+  required_version = ">= 1.14.0"
+
+  cloud {
+    organization = "za4emyavam"
+    
+    workspaces {
+      name = "portfolio"
+    }
+  }
+}
